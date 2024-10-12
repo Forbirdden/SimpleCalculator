5035c12611a3ef3cf7e835cb6a2eb6cbc08155e8
Forbirdden <102984166+Forbirdden@users.noreply.github.com>
2024-10-12 11:34:19 +0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -5,8 +5,8 @@ const path = require('node:path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 380,
+    height: 690,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
